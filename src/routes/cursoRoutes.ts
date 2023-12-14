529c0779b9fe6e5a6f8042d06200a9d4dd6bc6fa
Leonardo Ramírez <31749560+LeoRami99@users.noreply.github.com>
2023-12-14 17:01:21 -0500

    Se añade la ruta para todos los cursos

--- src/routes/cursoRoutes.ts
+++ src/routes/cursoRoutes.ts
@@ -6,10 +6,13 @@ const router = express.Router();
 const cursoController = new CursoController();
 
 // creación del curso
+router.get("/", cursoController.getCursos);
 router.post("/crear", cursoController.crearCurso);
 router.get("/modulos-lecciones", cursoController.getCursoModuloLecciones);
 router.put("/actualizar/:id_curso", cursoController.updateCurso);
 router.put("/actualizar-modulos-lecciones/:id_curso", cursoController.updateModulosLecciones);
+
+// obtener todos los cursos
 
 // Esta es para la los usuarios
 router.get("/modulos-lecciones-usuario", cursoController.getCursoModuloLeccionesUsuario);
